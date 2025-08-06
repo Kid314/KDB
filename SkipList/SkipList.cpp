847f90d39982cba05c677bb068474c723a318863
kid314 <2607644671@qq.com>
2025-08-06 11:03:48 +0800

    run continue

---
+++ SkipList/SkipList.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kid314 on 25-8-6.
+//
+
+#include "SkipList.h"
---
+++ SkipList/SkipList.h
@@ -0,0 +1,29 @@
+//
+// Created by kid314 on 25-8-6.
+//
+
+#ifndef SKIPLIST_H
+#define SKIPLIST_H
+
+#include <string>
+
+enum class status
+{
+    alive,
+    delete
+};
+class Node
+{
+public:
+    std::pair<std::string,std::string> key_value;
+
+private:
+
+};
+class SkipList
+{
+public:
+private:
+};
+
+#endif //SKIPLIST_H
