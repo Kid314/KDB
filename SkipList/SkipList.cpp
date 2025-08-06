669a903f4b9a9d809cab08419d015b3f509378b3
kid314 <2607644671@qq.com>
2025-08-06 17:35:20 +0800

    做跳表哈哈哈

--- SkipList/SkipList.cpp
+++ SkipList/SkipList.cpp
@@ -3,3 +3,80 @@
 //
 
 #include "SkipList.h"
+#include <utility>
+
+int SkipList::num_generate()
+{
+    int level=1;
+    while (dist(gen)==1&&level<max_level)
+    {
+        level++;
+    }
+    return level;
+}
+Node::Node(std::string&& new_key,std::string&& new_value, int node_level):key(std::move(new_key)),value(std::move(new_value)),level(node_level),status(Stat::Alive)
+{
+    forward.resize(node_level,nullptr);
+}
+std::optional<std::string_view> SkipList::get(const std::string& request_key) const
+{
+    Node* help=head;
+    for (int i=current_level-1;i>=0;--i)
+    {
+        while (help->forward[i]!=nullptr&&help->forward[i]->value<request_key)
+        {
+            help=help->forward[i];
+        }
+    }
+    help=help->forward[0];
+    if (help!=nullptr&&help->status!=Stat::Deleted&&help->key!=request_key)
+    {
+        return help->value;
+    }
+    return std::nullopt;
+}
+void SkipList::put(std::string& request_key, std::string& request_value)
+{
+    int temp_level=num_generate();
+    std::vector<Node*> mem_vec;
+    mem_vec.resize(temp_level);
+    Node* help=head;
+    for (int i=current_level-1;i>=0;--i)
+    {
+        while (help->forward[i]!=nullptr&&help->forward[i]->value<request_key)
+        {
+            help=help->forward[i];
+        }
+        mem_vec[i]=help;
+    }
+    if (temp_level>current_level)
+    {
+        for (int i=temp_level;i>current_level;--i)
+        {
+            mem_vec[i-1]=nullptr;
+        }
+        current_level=temp_level;
+    }
+    if ()
+    Node* new_node=new Node(std::move(request_key),std::move(request_value),num_generate());
+
+}
+void SkipList::remove(const std::string&)
+{
+
+}
+SkipList::SkipList(int new_max_level):max_level(new_max_level),current_level(1),gen(std::random_device{}()),dist(0,1)
+{
+    head=new Node("","",max_level);
+}
+SkipList::~SkipList()
+{
+    Node* current=head->forward[0];
+    while (current!=nullptr)
+    {
+        Node* next=current->forward[0];
+        delete current;
+        current=next;
+    }
+    delete head;
+}
--- SkipList/SkipList.h
+++ SkipList/SkipList.h
@@ -4,26 +4,41 @@
 
 #ifndef SKIPLIST_H
 #define SKIPLIST_H
-
+#include <string_view>
+#include <optional>
+#include <random>
 #include <string>
-
-enum class status
+#include <vector>
+enum class Stat
 {
-    alive,
-    delete
+    Alive,
+    Deleted
 };
 class Node
 {
 public:
-    std::pair<std::string,std::string> key_value;
-
-private:
-
+    std::vector<Node*> forward;
+    std::string key;
+    std::string value;
+    Stat status;
+    int level;
+    explicit Node(std::string&&,std::string&&,int);
 };
 class SkipList
 {
 public:
+    void put(std::string&,std::string&);
+    [[nodiscard]] std::optional<std::string_view> get(const std::string&) const;
+    void remove(const std::string&);
+    explicit SkipList(int);
+    ~SkipList();
 private:
+    [[nodiscard]] int num_generate();
+    int current_level;
+    int max_level;
+    Node* head;
+    std::mt19937 gen;
+    std::uniform_int_distribution<> dist;
 };
 
 #endif //SKIPLIST_H
